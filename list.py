7b051f2c29a212980ab62a371d6099a631d1ceb4
lalit4312 <lalit69056@gmail.com>
2022-05-27 09:45:47 +0545

    anish

---
+++ list.py
@@ -0,0 +1,83 @@
+#list
+# data=["sunil", "roshan", 29]
+# for i in data:
+#     print(i)
+
+
+#python list slicing and indexing(kunai indexing garda index value matra aauxa aru aaudaina )
+# data=["sunil", "roshan", 29]
+# print(data[1])
+
+
+#creating list using range()function
+# data= list(range(0,10,1))
+# print(data)
+
+#python append,extend (append=kunai ma value add garnu xa vane append use garera value place garda tyo value
+# chiyako ko thau ma aauxa  , extend=yo use garda value one by one execute hunxa jastai lalit=l,a,l,i,t)
+# data= ["sunil", "roshan", 29]
+# print(data)
+# data.extend('sunil')
+# data.append("programming")
+# print(data)
+
+#insert
+data=["sunil", "roshan", 29]
+print(data)
+data.insert(2,"battle")
+print(data)
+
+#inserting a tuple(as an element) to the list
+# mixed_list=[{1,2}, [5,6,7], {"a":"r"}]
+# number_tuple=(3,4)
+# mixed_list.insert(1,number_tuple)
+# print("updated list:", mixed_list)
+
+#updating the elements of the list(update=to modify something)
+# data=["sunil", "roshan", 29]
+# print(data)
+# data[0]=8
+# data[1]=10
+# data[2]=2
+# print(data)
+
+#python list del function
+# data=["sunil", "roshan", 29]
+# print(data)
+# del data[1]
+# print(data)
+
+#python list remove
+# data=["sunil", "roshan", 29]
+# print(data)
+# data.remove(29)
+# print(data)
+
+#python list pop(pop=last value lai remove garxa)
+# data=["sunil", "roshan", 29]
+# print(data)
+# data.pop()
+# print(data)
+
+#concatenation of two variable(duita lai jodney)
+# data=["sunil", "roshan", 29]
+# data2=["a","b",12]
+# print(data+data2)
+
+#python list copy(duplicate file create garney)
+# data=["sunil", "roshan", 29]
+# data2=[]
+# data2=data.copy()
+# print(data)
+# print(data2)
+
+#python list extend
+# data=["sunil", "roshan", 29]
+# data2=["a","b",12]
+# data2.extend(data)
+# print(data2)
+
+#python lisst clear
+# data=["sunil", "roshan", 29]
+# data.clear()
+# print(data)
